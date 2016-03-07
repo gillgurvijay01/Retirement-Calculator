6e36f3ca7849df5518ba53595491f2bca1feda72
Josh <you@example.coexit>
2016-03-07 14:31:48 -0600

    stage 1

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+var currentAge = prompt ('What is your current age?');
+
+var retireAge = prompt ('At what age would you like to retire?');
+
+var adjective = prompt ('Enter a adjective');
+
+var adverb = prompt ('Enter a adverb');
+
+ 
+
+
+//console.log('Do you '+verb+' your '+adjective+' '+noun+' '+adverb+'? That\'s hilarious');
+
+document.write('Do you '+verb+' your '+adjective+' '+noun+' '+adverb+'? That\'s hilarious');
+//console.log('You will be '+nounFirst+' years old a year from now');
+
+ 
