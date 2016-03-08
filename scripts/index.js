41eb71db7c8b47f6ca943c7e049c062a696956f5
Josh <you@example.coexit>
2016-03-07 20:18:00 -0600

    final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 var currentAge = prompt('How old are you now?');
 document.write('<div>You are '+currentAge+' years old.</div>');
 
-var retireAge = prompt('At what age do you want to retie?');
+var retireAge = prompt('At what age do you want to retire?');
 document.write('<div>You want to be '+retireAge+' years old when you retire.</div>')
 
 var retirement = parseFloat(retireAge - currentAge);
